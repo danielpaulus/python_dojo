d7ffd49d71d841a473ca921cbfedabd72f76bcff
Elisabeth Vonesch <vonesch.elisabeth@gmail.com>
2019-07-03 19:40:07 +0200

    fix bug in variable expected

--- src/simple/test_Medium01.py
+++ src/simple/test_Medium01.py
@@ -8,14 +8,14 @@ from src.simple.Medium01 import Medium01
 class Test(unittest.TestCase):
     def setUp(self):
         self.medium = Medium01()
-
+gi
     def test_make_me_pass_01(self):
         letters = string.ascii_lowercase
         random_string = ''.join(random.choice(letters) for i in range(10))
         random_number = random.randint(1, 100000)
 
         my_dictionary = {random_string: random_number}
-        expected = '{{"{}":{}}}'.format(random_string, random_number)
+        expected = '{{"{}": {}}}'.format(random_string, random_number)
 
         result = self.medium.make_me_pass01(my_dictionary)
         self.assertEqual(result, expected)
