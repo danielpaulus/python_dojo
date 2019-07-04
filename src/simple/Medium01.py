07a83c884231d87c5c7a3fde7b59be0f5a35e3a6
Elisabeth Vonesch <vonesch.elisabeth@gmail.com>
2019-07-04 12:39:06 +0200

    test if email valid

--- src/simple/Medium01.py
+++ src/simple/Medium01.py
@@ -2,5 +2,8 @@ class Medium01:
     def make_me_pass01(self, some_dictionary):
         return None
 
+    def make_me_pass02(self, another_dictionary):
+        return None
+
 
 
--- src/simple/test_Medium01.py
+++ src/simple/test_Medium01.py
@@ -1,6 +1,7 @@
 import unittest
 import random
 import string
+import re
 
 from src.simple.Medium01 import Medium01
 
@@ -19,3 +20,12 @@ class Test(unittest.TestCase):
 
         result = self.medium.make_me_pass01(my_dictionary)
         self.assertEqual(result, expected)
+
+    def test_make_me_pass_02(self):
+        letters = string.ascii_lowercase
+        fake_email = ''.join(random.choice(letters) for i in range(10))
+        my_login = {"email": fake_email, "password": 123456789}
+        email_regex = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'
+        test = re.search(my_login["email"], email_regex)
+        result = self.medium.make_me_pass02(my_login)
+        self.assertEqual(result, test)
