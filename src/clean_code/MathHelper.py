7992e462e25397d6539984aa9673c68ad7f8f6a0
Elisabeth Vonesch <vonesch.elisabeth@gmail.com>
2019-07-06 18:48:15 +0200

    implement function to find largest number

--- src/clean_code/MathHelper.py
+++ src/clean_code/MathHelper.py
@@ -1,3 +1,5 @@
+import sys
+
 class MathHelper:
 
     def find_largest_number_in_list(self, list_with_numbers):
@@ -6,6 +8,7 @@ class MathHelper:
         :param list_with_numbers:
         :return: largest number in list, -sys.maxsize -1 for empty list
         """
-        return 0
+        max_number = max(list_with_numbers, default=-sys.maxsize-1)
+        return max_number
 
 
