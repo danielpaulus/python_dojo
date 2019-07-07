2df34b0aca99c2c349edd376db2d929da7eea08e
Elisabeth Vonesch <vonesch.elisabeth@gmail.com>
2019-07-07 20:42:36 +0200

    implement test for capitalized anagrams

--- src/clean_code/test_StringHelper.py
+++ src/clean_code/test_StringHelper.py
@@ -7,20 +7,22 @@ class Test(unittest.TestCase):
 
     def setUp(self):
         self.string_helper =  StringHelper()
+        self.anagram1 = "mary"
+        self.anagram2 = "amry"
 
     def test_strings_are_anagrams_random_anagram(self):
-        example_string_1 = "mary"
-        example_string_2 = "amry"
-        expected = True
-        actual = self.string_helper.strings_are_anagrams(example_string_1, example_string_2)
-        self.assertEqual(expected, actual)
+        result = self.string_helper.strings_are_anagrams(self.anagram1, self.anagram2)
+        self.assertTrue(result)
 
     def test_strings_are_anagrams_empty_string(self):
         example_string_1 = "Here comes dots"
         example_string_2 = ""
-        expected = False
-        actual = self.string_helper.strings_are_anagrams(example_string_1, example_string_2)
-        self.assertEqual(expected, actual)
+        result = self.string_helper.strings_are_anagrams(example_string_1, example_string_2)
+        self.assertFalse(result)
 
-    #def test_strings_are_anagrams_anagram_capitalized(self)
+    def test_strings_are_anagrams_anagram_capitalized(self):
+        anagram_capitalized = "Amry"
+        result = self.string_helper.strings_are_anagrams(self.anagram1, anagram_capitalized)
+        self.assertTrue(result)
+
 
